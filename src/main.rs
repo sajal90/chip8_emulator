fcf4d68ec1454ba12ec495dbdb0523a9f57e75d2
Sajal Gupta <sajal2005gupta@gmail.com>
2026-06-20 00:19:04 +0530

    Implement load and fetch

--- src/main.rs
+++ src/main.rs
@@ -22,10 +22,28 @@ impl Cpu {
             memory: [0; MAX_MEMORY],
         }
     }
+
+    fn load_rom(&mut self, data: &[u8]) {
+        for (i, byte) in data.iter().enumerate() {
+            self.memory[0x200 + i] = *byte;
+        }
+    }
+
+    fn fetch(&mut self) -> u16 {
+        let fir = self.memory[self.pc as usize] as u16;
+        let sec = self.memory[self.pc as usize + 1] as u16;
+        self.pc += 2;
+        (fir << 8) | sec
+    }
 }
 
 
 fn main() {
-    let cpu = Cpu::new();
+    let mut cpu = Cpu::new();
+
+    let rom: &[u8] = &[0x00, 0xE0]; // fake rom for test
+    cpu.load_rom(rom);
+
+    let opcode = cpu.fetch();
     println!("Cpu started at {}", cpu.pc);
 }
